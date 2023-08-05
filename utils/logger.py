1d2b958aaa4e808a61b7d017c367124c833240f3
Wenda Zheng <dylan.d.zheng@gmail.com>
2023-08-05 17:56:10 +0200

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,14 @@
+import logging
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """Setup logger to output to console and log file."""
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
