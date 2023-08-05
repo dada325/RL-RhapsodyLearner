8209f58b11771de5d6d26f451accdadd8324bbd2
Wenda Zheng <dylan.d.zheng@gmail.com>
2023-08-05 04:37:06 +0200

    Update types.py

--- types.py
+++ types.py
@@ -1,1 +1,29 @@
 
+from typing import NamedTuple
+from abc import ABC, abstractmethod
+
+
+class Experience(NamedTuple):
+    state: State
+    action: Action
+    reward: float
+    next_state: State
+    done: bool
+
+
+class Batch(NamedTuple):
+    states: np.ndarray
+    actions: np.ndarray
+    rewards: np.ndarray
+    next_states: np.ndarray
+    dones: np.ndarray
+
+
+class Agent(ABC):
+    @abstractmethod
+    def act(self, state: State, explore: bool = True) -> Action:
+        pass
+
+    @abstractmethod
+    def learn(self, batch: Batch):
+        pass
